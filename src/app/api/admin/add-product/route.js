cfae22eb6c9c72b2f03aefd09b17b77f1949151a
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-08-16 13:18:27 +0200

    Update route.js

--- src/app/api/admin/add-product/route.js
+++ src/app/api/admin/add-product/route.js
@@ -1,4 +1,6 @@
 import connectToDB from "@/database";
+import Product from "@/models/product";
+import Joi from "joi";
 import { NextResponse } from "next/server";
 
 const AddNewProductSchema = Joi.object({
@@ -18,6 +20,60 @@ export const dynamic = "force-dynamic";
 export async function POST(req) {
   try {
     await connectToDB();
+
+    const user = "admin";
+
+    if (user === "admin") {
+      const extractData = await req.json();
+      const {
+        name,
+        description,
+        price,
+        imageUrl,
+        category,
+        sizes,
+        deliveryInfo,
+        onSale,
+        priceDrop,
+      } = extractData;
+
+      const { error } = AddNewProductSchema.validate({
+        name,
+        description,
+        price,
+        imageUrl,
+        category,
+        sizes,
+        deliveryInfo,
+        onSale,
+        priceDrop,
+      });
+
+      if (error) {
+        return NextResponse.json({
+          success: false,
+          message: error.details[0].message,
+        });
+      }
+      const newlyCreatedProduct = await Product.create(extractData);
+
+      if (newlyCreatedProduct) {
+        return NextResponse.json({
+          success: true,
+          message: "Product added successfully",
+        });
+      } else {
+        return NextResponse.json({
+          success: false,
+          message: "Failed to add the product ! please try again",
+        });
+      }
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "You are not autorized !",
+      });
+    }
   } catch (error) {
     console.log(error);
     return NextResponse.json({
