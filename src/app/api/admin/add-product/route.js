733f8a8990496dea25b5156520878a41c0903e71
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-08-16 12:55:33 +0200

    Update route.js

--- src/app/api/admin/add-product/route.js
+++ src/app/api/admin/add-product/route.js
@@ -1,6 +1,18 @@
 import connectToDB from "@/database";
 import { NextResponse } from "next/server";
 
+const AddNewProductSchema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  price: Joi.number().required(),
+  category: Joi.string().required(),
+  sizes: Joi.array().required(),
+  deliveryInfo: Joi.string().required(),
+  onSale: Joi.string().required(),
+  priceDrop: Joi.number().required(),
+  imageUrl: Joi.string().required(),
+});
+
 export const dynamic = "force-dynamic";
 
 export async function POST(req) {
